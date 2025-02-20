2d4f9e61c5f9a7476c365b13cac6c554e65da8b9
tub2105597 <tub2105597@student.ctu.edu.vn>
2025-02-20 22:07:51 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Opps, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ</router-link>
+        </p>
+    </div>
+</template>
